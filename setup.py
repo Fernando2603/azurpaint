b7913a58df7bc8b0645ca73ae9431169c15165dd
Fernando2603 <81207411+Fernando2603@users.noreply.github.com>
2024-08-27 14:56:29 +0700

    add pythonv version 3.9 and 3.10 to pyproject.toml

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
   name='azurpaint',
-  version='1.0.0',
   packages=find_packages(),
-  python_requires='>=3.9',
-  install_requires=['UnityPy', 'Pillow']
+  python_requires='>=3.9'
 )
