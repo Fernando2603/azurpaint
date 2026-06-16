71adaca87ca2db858bbca49a736fcc21c08cf8e6
Fernando2603 <81207411+Fernando2603@users.noreply.github.com>
2026-06-16 15:15:04 +0700

    upgrade UnityPy into 1.25.0

--- azurpaint/classes/AABB.py
+++ azurpaint/classes/AABB.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from .Vector2 import Vector2
 
 
@@ -11,15 +9,14 @@ class AABB:
     self.center = center
     self.extend = extend
 
-
   def __repr__(self) -> str:
-    return f"<{self.__class__.__name__} center={self.center.values()} extend={self.extend.values()}>"
-
+    return (
+      f"<{self.__class__.__name__} center={self.center.values()} extend={self.extend.values()}>"
+    )
 
   @property
   def padding(self) -> Vector2:
     return self.center - self.extend
-
 
   @property
   def size(self) -> Vector2:
